0862ef71c5b529f4ac2690340dcdcf87ab83a972
Bob Dowling <bobbydowling6@gmail.com>
2018-02-13 09:41:23 -0800

    Add project files.

---
+++ InnvoTech/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InnvoTech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
