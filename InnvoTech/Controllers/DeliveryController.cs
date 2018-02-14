3d1e28307feb513aef110a82cae946c7e1f1246b
Bob Dowling <bobbydowling6@gmail.com>
2018-02-14 14:45:42 -0800

    Added validation code

--- InnvoTech/Controllers/DeliveryController.cs
+++ InnvoTech/Controllers/DeliveryController.cs
@@ -12,19 +12,16 @@ namespace InnvoTech.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            ViewData["States"] = new string[] {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
-            "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
-            "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
-            "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", 
-            "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" };
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Index(DeliveryViewModel models)
-        {
-            return this.View(new {MyProperty = "MyValue" });
-
+        { if (ModelState.IsValid)
+            {
+                return this.RedirectToAction("Index", "Home");
+            }
+            return View();
         }
     }
 }
--- InnvoTech/Models/DeliveryViewModel.cs
+++ InnvoTech/Models/DeliveryViewModel.cs
@@ -9,15 +9,16 @@ namespace InnvoTech.Models
     public class DeliveryViewModel
     {
         [System.ComponentModel.DataAnnotations.Required]
-        [System.ComponentModel.DataAnnotations.Display(Name ="Your Address")]
+        [System.ComponentModel.DataAnnotations.Display(Name ="Primary Address")]
         public string Address { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
         public string City { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.RegularExpression(@"^\d{5}(?:[-\s]\d{4})?$")]
         public int Zip { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
         public string State { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
