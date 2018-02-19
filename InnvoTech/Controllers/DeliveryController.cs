497061d8302ca2b5f6e6d14f77437b75be494ff2
Bob Dowling <bobbydowling6@gmail.com>
2018-02-19 08:17:24 -0800

    updated code

--- InnvoTech/Controllers/DeliveryController.cs
+++ InnvoTech/Controllers/DeliveryController.cs
@@ -12,16 +12,34 @@ namespace InnvoTech.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            string gadgetName = "your cool gadget";
-            Request.Cookies.TryGetValue("productID", out gadgetName);
-            ViewData["gadgetName"] = gadgetName;
-
+            string cartId;
+            if (Request.Cookies.TryGetValue("cartId", out cartId))
+            {
+                byte[] gadgetBytes;
+                if(HttpContext.Session.TryGetValue(cartId, out gadgetBytes))
+                {
+                    System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                    {
+                        ms.Write(gadgetBytes, 0, gadgetBytes.Length);
+                        ms.Seek(0, System.IO.SeekOrigin.Begin);
+                        ViewData["gadgetName"] = (string)bf.Deserialize(ms);
+                    }
+                }
+            }
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Index(DeliveryViewModel models)
-        { if (ModelState.IsValid)
+        {
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"^\d{5}(?:[-\s]\d{4})?$");
+            if (string.IsNullOrEmpty(models.Zip) || !regex.IsMatch(models.Zip))
+            {
+                ModelState.AddModelError("Zip", "The zip code is invalid!");
+            }
+
+            if (ModelState.IsValid)
             {
                 return this.RedirectToAction("Index", "Home");
             }
