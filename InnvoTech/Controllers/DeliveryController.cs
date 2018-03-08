ee0dc949cb90bd76d3738d54aa9e7e46c5aa1398
Bob Dowling <bobbydowling6@gmail.com>
2018-03-08 10:26:05 -0800

    Added a recipt page

--- InnvoTech/Controllers/DeliveryController.cs
+++ InnvoTech/Controllers/DeliveryController.cs
@@ -120,6 +120,10 @@ namespace InnvoTech.Controllers
             }
                 return View();
             }
+            public IActionResult Recipt()
+            {
+                return View();
+            }
         }
     }
 
