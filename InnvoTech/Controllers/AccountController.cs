b9669759d5225b981edd581757a4410fbe09688e
Bob Dowling <bobbydowling6@gmail.com>
2018-03-12 11:35:45 -0700

    made changes

--- InnvoTech/Controllers/AccountController.cs
+++ InnvoTech/Controllers/AccountController.cs
@@ -87,7 +87,7 @@ namespace InnvoTech.Controllers
                         ModelState.AddModelError(error.Code, error.Description);
                     }
                 }
-                RedirectToAction("Account", "Login");
+               return RedirectToAction("Account", "Login");
             }
             return View();
 
